49ca59bdb3b68ae0d8aa4f36596a6017bedfe535
RIOU Kevin <kevin@riou.pro>
2022-03-08 00:06:44 +0100

    ✨feat(Animations): update animation on my head load

--- components/functionnal/Me/Me.tsx
+++ components/functionnal/Me/Me.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Image from "next/image";
 
 import Mee from "../../../assets/mee.png?lqip&blur";
@@ -9,18 +9,35 @@ interface IProps {}
 
 const MEE_SIZE = 500;
 
+const DELAY = 1500;
+
 export const Me: FC<IProps> = (props) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [timeFromMount] = useState<number>(new Date().getTime());
+
+  const handleLoaded = () => {
+    const actualTime = new Date().getTime();
+    const timeElapsed = actualTime - timeFromMount;
+    setTimeout(
+      () => {
+        setIsLoading(false);
+      },
+      timeElapsed < DELAY ? DELAY - timeElapsed : 1
+    );
+  };
+
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${isLoading ? styles.isLoading : ""}`}>
       <div className={styles.name}>RIOU</div>
       <Image
-        alt=""
+        alt="profilePic"
         blurDataURL={Mee.dataURI}
         className={styles.mee}
         height={MEE_SIZE}
         placeholder="blur"
         src={Mee.src}
         width={(MEE_SIZE / 3) * 2}
+        onLoadingComplete={handleLoaded}
       />
       <div className={styles.surname}>Kevin</div>
     </div>
