7a8cf36518a3d4693ec3baf09bea08d2bbff0096
RIOU Kevin <kevin@riou.pro>
2022-09-04 17:51:41 +0200

    😎 Feature: Change color for dark themes of toggle technical infos

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -18,17 +18,17 @@ export const Toggle: FC<IProps> = (props) => {
   return (
     <label
       htmlFor="red-toggle"
-      className="inline-flex relative items-center mr-5 cursor-pointer"
+      className="relative mr-5 inline-flex cursor-pointer items-center"
     >
       <input
         type="checkbox"
         id="red-toggle"
-        className="sr-only peer"
+        className="peer sr-only"
         checked={toggled}
         onChange={handleChange}
       />
-      <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-0 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary"></div>
-      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+      <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-0.5 after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-primary peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-0 dark:bg-gray-700"></div>
+      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-900">
         {label}
       </span>
     </label>
