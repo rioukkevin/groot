5ec30f0965ba7f79d812b5810bc13786102489ea
RIOU Kevin <riou.kkevin@gmail.com>
2024-10-13 22:37:24 +0200

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,13 @@ import { RootPage } from "@/modules/Root";
 import { Screen } from "@/modules/Screen";
 import IMGBackground from "@/assets/background.jpg";
 import { BackgroundProvider } from "@/modules/Theme/Background";
-import { ThemeColorProvider } from "@/modules/Theme/ThemeColor";
 
 export default function Home() {
   return (
     <BackgroundProvider defaultBackground={IMGBackground.src}>
-      <ThemeColorProvider>
-        <Screen>
-          <RootPage />
-        </Screen>
-      </ThemeColorProvider>
+      <Screen>
+        <RootPage />
+      </Screen>
     </BackgroundProvider>
   );
 }
