0da26a72534b46dc032bf4e42fb3492caebcf1f8
RIOU Kevin <riou.kkevin@gmail.com>
2026-01-17 21:06:41 +0100

    Basics

---
+++ src/modules/models/Models.tsx
@@ -0,0 +1,13 @@
+import type { FC } from "react"
+
+interface ModelsProps {
+  name: string
+}
+
+export const Models: FC<ModelsProps> = ({ name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  )
+}
